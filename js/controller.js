e1ee8dfe32df5f08aa38c344fde9ba014b30d48a
hiko_hitokiri <lastvnm@gmail.com>
2014-10-14 22:11:50 -0600

    controles filtros
    se logro la cracion de los dos controles en un mismo archivo para una misma ap
    haicendo el filtro para el contenido de los blogs y reduciendo la carga
    total de  la pagina

---
+++ js/controller.js
@@ -0,0 +1,46 @@
+var PortafolioBlog = angular.module('PortafolioBlog', ["ngRoute"]).
+filter('quitar', function() {
+    return function(input) {
+      input = input || '';
+      var out = "";
+      out = input.replace(/<[^>]*>/g, '');
+      out = out.replace(/&nbsp;/g, '');
+      // conditional based on optional argument
+
+      return out;
+    };
+  }).
+controller('BlogCtrl', ['$scope','$http', function ($scope,$http) {
+	$scope.method = 'GET';
+
+	$scope.url2 = 'https://www.googleapis.com/blogger/v3/blogs/7610380627270839038/posts?userIp=190.99.25.97&key=AIzaSyCZsHhv72zpjDni3ZT1Sw6uGIGEQCmgqCw';
+	$http({method: $scope.method, url: $scope.url2}).success(function(data){
+
+		$scope.datos1 = data;
+	});
+
+	// $scope.datosResourse = dataResource();
+}])
+
+var Portafolio = angular.module('Portafolio', ["ngRoute"]);
+
+Portafolio.controller('TrabajoCtrl', ['$scope','$http', function ($scope,$http) {
+	$scope.method = 'GET';
+	$scope.url = 'https://api.github.com/users/hitokiri/repos';
+	$http({method: $scope.method, url: $scope.url}).success(function(data){
+		$scope.datos = data;
+	});
+
+	// $scope.datosResourse = dataResource();
+}]);
+
+
+// Portafolio.factory('dataResource', [function ($resource) {
+// 	$scope.method = 'GET';
+// 	$scope.url = 'https://api.github.com/users/hitokiri/repos';
+// 	$http({method: $scope.method, url: $scope.url}).success(function(data){
+// 		$scope.datos = data;
+// 	return data;
+// 	})
+// }])
+
--- js/rss.js
+++ js/rss.js
@@ -1,21 +1,37 @@
-$(function(){
-	jQuery.fn.RssPorta = function(url) {
-		this.each(function() {
-			var $elemento = $(this);
-			var imagen = '';
-			var titulo = '';
-			var contenido = '';
-			var link = '';
-			$.getJSON(url, function(data) {
-				$.each(data.items, function(index, el) {
-					titulo = '<h3>' + el.title + '</h3>';
-					link = '<br><a href="' + el.url + '" class="btn btn-primary"> Ver el Post</a>'
-					contenido += "<div class='list-group-item post-active'>" + titulo + link + "</div>";
+// $(function(){
+// 	jQuery.fn.RssPorta = function() {
+// 		this.each(function() {
+// 			var $elemento = $(this);
+// 			var imagen = '';
+// 			var titulo = '';
+// 			var contenido = '';
+// 			var link = '';
+// 			var texto = '';
+// 			var url = 'https://www.googleapis.com/blogger/v3/blogs/7610380627270839038/posts?userIp=190.99.25.97&key=AIzaSyCZsHhv72zpjDni3ZT1Sw6uGIGEQCmgqCw';
+// 			$.getJSON(url, function(data) {
+// 				$.each(data.items, function(index, el) {
+// 					titulo = '<h3>' + el.title + '</h3>';
+// 					link = '<br><a href="' + el.url + '" class="btn btn-primary" target="_blank"> Ver el Post</a>';
+// 					texto = '<div class="texto-post">' + el.content + '</div>';
+// 					contenido += "<div class='list-group-item post-active'>" + titulo + texto + link + "</div>";
+// 				});
 
-
-				});
-				$elemento.html(contenido);
-			});
-		});
-	}
-});
+// 				$elemento.html(contenido);
+// 			});
+// 		});
+// 	}
+// 	jQuery.fn.Github = function(){
+// 		this.each(function() {
+// 			var $contenido = $(this);
+// 			var titulo = '';
+// 			var link = '';
+// 			var descripcion = '';
+// 			var url = 'https://api.github.com/users/hitokiri/repos'
+// 			$.getJSON(url, function(data) {
+// 				$.each(data, function(index, val) {
+// 					console.log(val.name);
+// 				});
+// 			});
+// 		});
+// 	}
+// });
