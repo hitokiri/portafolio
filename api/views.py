4635bf1175a0f80fa1d9160a5de9ea8ac164c7d7
hiko_hitokiri <lastvnm@gmail.com>
2014-10-30 03:18:01 -0600

    mejorado dj
    se logro mejorar el resultado alcanzado con angular y js en general

--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from django.template import RequestContext
+import urllib2
+import json
 
 # Create your views here.
 
 def view_index(request):
 	vista = 'index'
-	ctx = {'vista': vista}
+	response_trabajo = urllib2.urlopen('https://api.github.com/users/hitokiri/repos')
+	data_trabajo = json.load(response_trabajo)
+	response_blog = urllib2.urlopen('https://www.googleapis.com/blogger/v3/blogs/7610380627270839038/posts?userIp=190.99.25.97&key=AIzaSyCZsHhv72zpjDni3ZT1Sw6uGIGEQCmgqCw')
+	data_blog = json.load(response_blog)
+	ctx = {'vista': vista, 'trabajo': data_trabajo, 'blog':data_blog}
 	return render_to_response('index.html', ctx, context_instance=RequestContext(request))
